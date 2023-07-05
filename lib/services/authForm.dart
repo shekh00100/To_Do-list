de1c386f8aa2e6859c9925b8f974ff8ab5a2f940
shekh <imranshekh640@gmail.com>
2023-07-05 13:01:23 +0530

    Auth form

--- lib/services/authForm.dart
+++ lib/services/authForm.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AuthForm extends StatefulWidget {
   const AuthForm({super.key});
@@ -10,21 +11,140 @@ class AuthForm extends StatefulWidget {
 class _AuthScreenState extends State<AuthForm> {
   @override
   final _formkey = GlobalKey<FormState>(); //validation of form data
-  final email = '';
-  final password = '';
+  var email = '';
+  var password = '';
+  var username = '';
+
+  //--------------------//
+  startAuthentication()  {
+    final validity = _formkey.currentState!.validate();
+    FocusScope.of(context).unfocus();
+    if (validity) {
+      _formkey.currentState!.save();
+      submitform();
+    }
+  }
+  submitform()async{
+
+  }
+
+  bool isLoginPage = false;
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      child: ListView(
-        children: [
-          Container(
-              child: Form(
-            child: Column(
-              children: [],
-            ),
-          ))
-        ],
+    return Padding(
+      padding: const EdgeInsets.only(top: 100, bottom: 100),
+      child: Container(
+        padding: EdgeInsets.only(left: 10, right: 10, top: 20),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: ListView(
+          children: [
+            Container(
+                child: Form(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  if (!isLoginPage)
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      key: ValueKey('usermane'),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Incorrect UserName";
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        username = value!;
+                      },
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide()),
+                        labelText: "Enter UserName",
+                        labelStyle: GoogleFonts.roboto(),
+                      ),
+                    ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    key: ValueKey('email'),
+                    validator: (value) {
+                      if (value!.isEmpty || !value.contains('@')) {
+                        return "Incorrect Email";
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      email = value!;
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide()),
+                      labelText: "Enter Email",
+                      labelStyle: GoogleFonts.roboto(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    key: ValueKey('password'),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Incorrect Password";
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      password = value!;
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide()),
+                      labelText: "Enter Password",
+                      labelStyle: GoogleFonts.roboto(),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    height: 73,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: isLoginPage
+                          ? Text(
+                              "Login",
+                              style: GoogleFonts.roboto(),
+                            )
+                          : Text('Sign Up', style: GoogleFonts.roboto()),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          isLoginPage = !isLoginPage;
+                        });
+                      },
+                      child: isLoginPage
+                          ? Text('Not an User')
+                          : Text("Already a user?"),
+                    ),
+                  )
+                ],
+              ),
+            ))
+          ],
+        ),
       ),
     );
   }
